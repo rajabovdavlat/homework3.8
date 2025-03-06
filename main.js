9748eb2c553b3dcc3dd4e0996cec76a91cce5f47
rajabovdavlat <rajabovoff1@gmail.com>
2025-03-07 00:35:59 +0500

    qwe

--- main.js
+++ main.js
@@ -1,14 +1,76 @@
 // ============
-// 1-masala:
-//
+// 1-masala:Foydalanuvchi ob-havo gradusini kiritsin.
+// Agar gradus -20 dan past bo'lsa "Qahraton"
+// Agar gradus 0 va -20 oralig'ida bo'lsa "Juda sovuq"
+// Agar gradus 0 va 10 oralig'ida bo'lsa "Sovuq havo"
+// Agar gradus 10 va 20 oralig'ida bo'lsa "Salqin havo"
+// Agar gradus 20 va 30 oralig'ida bo'lsa "Iliq havo"
+// Agar gradus 30 va 40 oralig'ida bo'lsa "Issiq havo"
+// Agar gradus 40 dan baland bo'lsa "Jazirama" alertda chiqsin
+
+const harorat = parseInt(prompt("gradusni kiriting"));
+
+if (harorat < -20) {
+  console.log("Qahraton");
+} else if (harorat >= -20 && harorat < 0) {
+  console.log(`Juda sovuq`);
+} else if (harorat >= 0 && harorat < 10) {
+  console.log("Sovuq havo");
+} else if (harorat >= 10 && harorat < 20) {
+  console.log("Salqin havo");
+} else if (harorat >= 20 && harorat < 30) {
+  console.log("Iliq havo");
+} else if (harorat >= 30 && harorat < 40) {
+  console.log("Isiq havo");
+} else if (harorat >= 40) {
+  alert("Jazirama");
+}
 
 // ============
-// 2-masala:
-//
+// 2-masala:Sonni 3 ga va 5 ga boвЂlinishini tekshirish
+// Masala: Foydalanuvchi son kiritadi. Quyidagi shartlarni tekshirib natija chiqaring:
+// Agar son 3 ga boвЂlinsa, "Fizz" chiqsin.
+// Agar son 5 ga boвЂlinsa, "Buzz" chiqsin.
+// Agar ikkalasiga ham boвЂlinsa, "FizzBuzz" chiqsin.
+// Agar hech qaysisiga boвЂlinmasa, oвЂzini chiqarish.
+
+// const num = parseInt(prompt("Son kiriting"));
+// if (num % 3 == 0) {
+//   console.log(`Fizz`);
+// } else if (num % 5 == 0) {
+//   console.log(`Buzz`);
+// } else if (num % 3 == 0 && num % 5 == 0) {
+//   console.log("FizzBuzz");
+// } else {
+//   console.log(num);
+// }
 
 // ============
-// 3-masala:
-//
+// 3-masala:Calculator
+// Foydalanuvchidan 3 marta prompt orqali so'rov so'ralsin.
+// 1-sonni va 2-sonni keyin operatorni so'rasin.
+// operator + ga teng bo'lsa 1-son bilan 2-sonni qo'shib alertda chiqaring.
+// qolgan operatorlar ham huddi shunaqa..
+
+// const num1 = parseInt(prompt("Brinchi soni kiriting"));
+// const num2 = parseInt(prompt("Ikinchi soni kiriting"));
+// const operator = prompt("Amalni kiriting (+, -, *, /):");
+
+// if (operator == "+") {
+//   alert(`Natija: ${num1 + num2}`);
+// } else if (operator === "-") {
+//   alert(`Natija: ${num1 - num2}`);
+// } else if (operator === "*") {
+//   alert(`Natija: ${num1 * num2}`);
+// } else if (operator === "/") {
+//   if (num2 === 0) {
+//     alert("0 ga bo‘lish mumkin emas!");
+//   } else {
+//     alert(`Natija: ${num1 / num2}`);
+//   }
+// } else {
+//   alert("Noto‘g‘ri operator kiritildi!");
+// }
 
 // ============
 // 4-masala:
